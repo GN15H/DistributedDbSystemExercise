226909df1f1890fe3d4aa35554c6191871817913
Cristobal Ochoa Cadavid <122373889+GN15H@users.noreply.github.com>
2025-03-08 22:05:52 -0500

    Delete Cliente directory

--- Cliente/person.py
+++
@@ -1,6 +0,0 @@
-class Person:
-    def __init__(self,name, last_name, email, phone):
-        self.name = name
-        self.last_name = last_name
-        self.email = email
-        self.phone = phone
--- Cliente/request_model.py
+++
@@ -1,6 +0,0 @@
-class Request_M:
-    def __init__(self, sender, operation, person_data, time):
-        self.sender = sender
-        self.operation = operation
-        self.person_data = person_data
-        self.time = time
